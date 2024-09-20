616a7b9a251a162e49b7721584e7b13eac0257e2
Miles-Wiser <129561396+Miles-Wiser@users.noreply.github.com>
2024-09-20 14:11:11 -0600

    Update ball_in_circle.py
    -fixed minor issues i.e. ball.update() --> turtle.update()
    -added "collision" for the ball against the boundry

--- ball_in_circle.py
+++ ball_in_circle.py
@@ -4,7 +4,7 @@ import math
 #Draw Border
 border = turtle.Turtle()
 border.ht()
-border.tracer(0)
+turtle.tracer(0)
 
 def draw_circle():
   border.penup()
@@ -14,7 +14,7 @@ def draw_circle():
     border.fd(35.5)
     border.left(8)
   
-  border.update()
+  turtle.update()
   
   return (35.5 / math.sin(math.radians(8)))
 
@@ -35,15 +35,16 @@ def draw_ball():
   ball.end_fill()
   ball.penup()
   
-  ball.update()
+  turtle.update()
 
-ball_hspd = 8
-ball_vspd = 0
+ball_hspd = 1
+ball_vspd = 2
 
 def move_ball():
-  global ball_hspd
-  if(-radius >= ball.xcor() or ball.xcor() >= radius):
+  global ball_hspd, ball_vspd
+  if(math.sqrt((ball.xcor() ** 2) + (ball.ycor() ** 2)) >= 255):
     ball_hspd = -ball_hspd
+    ball_vspd = -ball_vspd
   return 0
 
 #Main Loop
